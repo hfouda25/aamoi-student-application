2ce2c7ba2d40760e4fc70c973d48015ba71f2083
hfouda25 <foudahamada@yahoo.com>
2026-01-11 21:04:57 -0400

    Add files via upload

---
+++ config/courses.ts
@@ -0,0 +1,33 @@
+
+export const COC_PROGRAMS = [
+  "OICNW Reg. II/1",
+  "Master/Chief Mate Reg. II/2",
+  "OICEW Reg. III/1",
+  "Chief/Second Engineer Reg. III/2",
+  "A.B Reg. II/4",
+  "A.B Reg. II/5",
+  "Oiler Reg. III/4"
+];
+
+export const SHORT_COURSES = [
+  "Basic Training (PST, FPFF, EFA, PSSR)",
+  "Proficiency in Survival Craft & Rescue Boats (PSCRB)",
+  "Advanced Fire Fighting (AFF)",
+  "Medical First Aid (MFA)",
+  "Medical Care (MC)",
+  "Security Awareness (SA)",
+  "Designated Security Duties (DSD)",
+  "Ship Security Officer (SSO)",
+  "Crowd Management",
+  "Crisis Management & Human Behaviour",
+  "Bridge Resource Management (BRM)",
+  "Engine Room Resource Management (ERM)",
+  "Radar Observer",
+  "ARPA",
+  "GMDSS",
+  "Tanker Familiarization (Oil/Chemical/LPG)",
+  "High Voltage"
+];
+
+export const STUDY_MODES = ["Full-time", "Part-time"];
+export const DELIVERY_MODES = ["In-person", "Online", "Blended"];
