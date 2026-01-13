9477f23b38a67e1efab4dc86634b3eae226544df
hfouda25 <foudahamada@yahoo.com>
2026-01-13 09:33:34 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>AA MOI Student Application</h1>
+      <p>React is now rendering correctly.</p>
+    </div>
+  );
+}
