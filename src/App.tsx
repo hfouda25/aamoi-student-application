d6b59045a9b586bffa45719b483b1cc80d8aecc5
hfouda25 <foudahamada@yahoo.com>
2026-01-13 10:03:26 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,4 +34,12 @@ const App: React.FC = () => {
   );
 };
 
-export default App;
+export default function App() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>AA MOI Student Application</h1>
+      <p>React is now rendering correctly.</p>
+    </div>
+  );
+}
+
