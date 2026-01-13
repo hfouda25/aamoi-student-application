d0be24e0a437de76ca2f1c353fe5a519b3209cf9
hfouda25 <foudahamada@yahoo.com>
2026-01-13 10:01:18 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("Root element not found");
-}
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
